d6bac6fb59c4bfc942be302b69b06b6f6d807e1f
DiSSNiKO <nikokalanda@gmail.com>
2022-08-03 17:43:23 +0400

    TokenomicsNice

--- JS/Script.js
+++ JS/Script.js
@@ -1,2 +1,12 @@
-const statbar = document.querySelectorAll(".statDecorNotLast");
-console.log('nunito');
+const statbar = document.querySelectorAll('.statDecorNotLast');
+let rgba = [211, 0, 100];
+let indx = 0;
+for (const decordiv of statbar) {
+    decordiv.style.backgroundColor = `rgba(${rgba[0]},${rgba[1]}, ${rgba[2]})`
+    indx += 1;
+    indx > 0 ? rgba[0] = 248 : null;
+    if (indx > 1) {
+        rgba[1] += 33;
+        rgba[2] += 17;
+    }
+}
